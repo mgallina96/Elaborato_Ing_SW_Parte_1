e783684565ba17c6fcccc7db4c761c8497175d27
Manuel Gallina <mgallina96@gmail.com>
2018-03-08 14:26:17 +0100

    Start Project

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,17 +1,8 @@
 package main;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
-
-public class Main extends Application {
-
-    @Override
-    public void start(Stage primaryStage) {
-
-    }
-
+public class Main {
 
     public static void main(String[] args) {
-        launch(args);
+
     }
 }
