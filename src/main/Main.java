25937d9fd428d019cc6b69ac3e07c645406261ff
giosuefilippini <giosu.filippini@gmail.com>
2018-03-08 12:37:50 +0100

    Modificata la classe utente
    Getter, setter e campi

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,10 +3,19 @@ package main;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        GregorianCalendar birthday = new GregorianCalendar();
+        birthday.set(2000, Calendar.MARCH, 4);
+        User user = new User("AAA", "BBB", "CCC", "password", birthday);
+        System.out.println(user.toString());
+
+        System.out.println(user.isOfAge() ? "Maggiorenne" : "Non maggiorenne");
     }
 
 
--- src/main/User.java
+++ src/main/User.java
@@ -1,14 +1,69 @@
 package main;
 
+import java.time.Year;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 public class User {
 
+    private static final long OF_AGE = 568024668000L;
     private String username;
+    private String firstName;
+    private String lastName;
+    private String password;
+    private GregorianCalendar birthday;
+    private GregorianCalendar subscriptionDate;
+    private GregorianCalendar expiryDate;
+    boolean isExpired;
+    boolean isUser;
 
-    public User() {
-
+    public User(String firstName, String lastName, String username, String password, GregorianCalendar birthday) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+        this.birthday = birthday;
+        subscriptionDate = new GregorianCalendar();
+        expiryDate = (GregorianCalendar)(subscriptionDate.clone());
+        expiryDate.add(Calendar.YEAR, 5);
+        System.out.println(subscriptionDate.toZonedDateTime());
+        System.out.println(expiryDate.toZonedDateTime());
     }
 
     public String getUsername() {
         return username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public GregorianCalendar getBirthday() {
+        return birthday;
+    }
+
+    public GregorianCalendar getSubscriptionDate() {
+        return subscriptionDate;
+    }
+
+    public GregorianCalendar getExpiryDate() {
+        return expiryDate;
+    }
+
+    public boolean isOfAge() {
+        return subscriptionDate.getTimeInMillis() - birthday.getTimeInMillis() >= OF_AGE;
+    }
+
+    public String toString() {
+        return String.format("First name: %s | Last name: %s | Username: %s | Birthday = %s\n", firstName, lastName, username, birthday.toZonedDateTime());
     }
 }
