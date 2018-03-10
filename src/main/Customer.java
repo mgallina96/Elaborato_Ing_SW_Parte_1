cc56c30f157c94f6e6fbda9d1c0c16b19c27f00e
Alessandro Polcini <alessandropolcini@gmail.com>
2018-03-10 15:36:15 +0100

    Leggero cambiamento, potrebbe funzionare?
    Ho reso User una classe padre con due figlie: Operator e Customer. Customer è il cliente e basta. Secondo me tenere tutto in User e gestire la differenza cliente/operatore mediante un booleano diventa troppo confusionario non appena il programma si complica un po', poi ditemi voi.
    
    Ho fatto una classe test ignorante per provare a vedere come gira il database e sembra funzionare tutto.

---
+++ src/main/Customer.java
@@ -0,0 +1,42 @@
+package main;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class Customer extends User {
+
+    private static final long OF_AGE = 568024668000L;
+    private static final int EXPIRY_TIME_IN_YEARS = 5;
+    private GregorianCalendar subscriptionDate;
+    private GregorianCalendar expiryDate;
+    private boolean isExpired;
+
+    public Customer(String firstName, String lastName, String username, String password, GregorianCalendar birthday) {
+        super(firstName, lastName, username, password, birthday);
+        super.setCustomer(true);
+
+        subscriptionDate = new GregorianCalendar();
+        expiryDate = (GregorianCalendar)(subscriptionDate.clone());
+        expiryDate.add(Calendar.YEAR, EXPIRY_TIME_IN_YEARS);
+    }
+
+    public boolean isOfAge() {
+        return subscriptionDate.getTimeInMillis() - super.getBirthday().getTimeInMillis() >= OF_AGE;
+    }
+
+    public GregorianCalendar getSubscriptionDate() {
+        return subscriptionDate;
+    }
+
+    public void setSubscriptionDate(GregorianCalendar subscriptionDate) {
+        this.subscriptionDate = subscriptionDate;
+    }
+
+    public GregorianCalendar getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(GregorianCalendar expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+}
---
+++ src/main/MainTest.java
@@ -0,0 +1,60 @@
+package main;
+import java.io.File;
+import java.util.*;
+
+public class MainTest {
+
+    static Database database = Database.getInstance();
+    static ArrayList<String> nomi = setNomiECognomi("nomi_italiani.txt");
+    static ArrayList<String> cognomi = setNomiECognomi("cognomi_italiani.txt");
+    public static final int HOW_MANY = 4000;
+
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int lenN = nomi.size();
+        int lenC = cognomi.size();
+
+        for(int i = 0; i < HOW_MANY; i++) {
+            String nome = nomi.get(rand.nextInt(lenN));
+            String cognome = cognomi.get(rand.nextInt(lenC));
+            String username = nome + "_" + cognome + "_" + rand.nextInt(Integer.MAX_VALUE);
+            String password = rand.nextLong() + "";
+            GregorianCalendar gregorianCalendar = new GregorianCalendar();
+            gregorianCalendar.set(rand.nextInt(65) + 1935, rand.nextInt(12), rand.nextInt(30) + 1);
+
+            //ci saranno molti più clienti che non operatori ovviamente
+            User o = (rand.nextDouble() > 0.9) ?
+                    new Operator(nome, cognome, username, password, gregorianCalendar) :
+                    new Customer(nome, cognome, username, password, gregorianCalendar);
+
+            database.addUser(o);
+        }
+
+        //uso values() perchè è un metodo che ritorna una collection "stream"abile, la hash map non lo è purtroppo
+        database.getUserList().values().stream()
+                //.filter(s -> !s.isCustomer())
+                .forEach(s -> System.out.println(s.toString() + "\t\tCustomer? " + s.isCustomer() + "\n"));
+
+    }
+
+    public static ArrayList<String> setNomiECognomi(String pathName) {
+        ArrayList<String> a = new ArrayList<>();
+        File f = new File(pathName);
+
+        try {
+            Scanner fs = new Scanner(f);
+
+            while(fs.hasNextLine()) {
+                a.add(fs.nextLine());
+            }
+
+            fs.close();
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+
+        return a;
+    }
+
+}
