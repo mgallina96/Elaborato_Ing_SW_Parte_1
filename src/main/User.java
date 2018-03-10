32e529a887f8af683c1d1aba1aa304ea400d409c
Alessandro Polcini <alessandropolcini@gmail.com>
2018-03-10 13:00:22 +0100

    Leggero cambiamento, potrebbe funzionare?
    Ho reso User figlia di Operator, così può differenziarsi e specificarsi implementando i metodi che Operator non può implementare.

--- src/main/User.java
+++ src/main/User.java
@@ -4,50 +4,20 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 
-public class User {
+public class User extends Operator {
 
     private static final long OF_AGE = 568024668000L;
-    private String username;
-    private String firstName;
-    private String lastName;
-    private String password;
-    private GregorianCalendar birthday;
     private GregorianCalendar subscriptionDate;
     private GregorianCalendar expiryDate;
     boolean isExpired;
-    boolean isUser;
 
     public User(String firstName, String lastName, String username, String password, GregorianCalendar birthday) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.username = username;
-        this.password = password;
-        this.birthday = birthday;
+        super(firstName, lastName, username, password, birthday);
+        super.setUser(true);
+
         subscriptionDate = new GregorianCalendar();
         expiryDate = (GregorianCalendar)(subscriptionDate.clone());
         expiryDate.add(Calendar.YEAR, 5);
-        System.out.println(subscriptionDate.toZonedDateTime());
-        System.out.println(expiryDate.toZonedDateTime());
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public GregorianCalendar getBirthday() {
-        return birthday;
     }
 
     public GregorianCalendar getSubscriptionDate() {
@@ -59,10 +29,7 @@ public class User {
     }
 
     public boolean isOfAge() {
-        return subscriptionDate.getTimeInMillis() - birthday.getTimeInMillis() >= OF_AGE;
+        return subscriptionDate.getTimeInMillis() - super.getBirthday().getTimeInMillis() >= OF_AGE;
     }
 
-    public String toString() {
-        return String.format("First name: %s | Last name: %s | Username: %s | Birthday = %s\n", firstName, lastName, username, birthday.toZonedDateTime());
-    }
 }
